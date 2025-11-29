bbb2930389c966bb5481474b3cd595107cbfab30
Jis George <jis.george@soti.net>
2025-11-29 22:40:01 +0530

    App v2, fixed button alignments in MainActivity layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.sanchari.bus"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         // --- RELEASE BUILD OPTIMIZATION ---
         // Uncomment this block when generating a Release APK.
